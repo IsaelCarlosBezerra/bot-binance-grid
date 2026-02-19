57dbc2c66a73d4ef8150db566ce9e168f9069636
IsaelCarlosBezerra <isaelc.bezerra@hotmail.com>
2026-02-19 13:04:49 -0300

    correção de função que calibra preço de compra

--- src/core/utils/calibrarPrecoDeCompra.ts
+++ src/core/utils/calibrarPrecoDeCompra.ts
@@ -1,10 +1,12 @@
 import { BotConfig } from "../../config/bot.config.js"
+import { priceBuffer } from "../price-buffer.js"
 import { strategyState } from "../strategy-state.js"
 
 export function calibrarPrecoCompra() {
-	const precoAtual = 0
-	const precoCompraAtual = 0
-	const diferenca = (precoAtual - precoCompraAtual / precoAtual) * 100
+	const precoAtual = priceBuffer.getPrice()
+	const precoCompraAtual = strategyState.nextBuyPrice
+	const diferenca = ((precoAtual - precoCompraAtual) / precoAtual) * 100
+
 	if (diferenca > 1) {
 		const newPrecoCompra = precoAtual * (1 - BotConfig.dropPercentage)
 		strategyState.nextBuyPrice = newPrecoCompra
