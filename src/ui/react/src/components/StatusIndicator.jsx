cc6785ee1af3058e1590ecf19c9a82c331d8df3f
IsaelCarlosBezerra <isaelc.bezerra@hotmail.com>
2026-02-26 23:32:47 -0300

    Deploy

--- src/ui/react/src/components/StatusIndicator.jsx
+++
@@ -1,8 +0,0 @@
-export default function StatusIndicator({ enabled }) {
-	return (
-		<div className="status">
-			<div className={`dot ${enabled ? "on" : "off"}`}></div>
-			<div>{enabled ? "Bot em execução" : "Bot desligado"}</div>
-		</div>
-	)
-}
